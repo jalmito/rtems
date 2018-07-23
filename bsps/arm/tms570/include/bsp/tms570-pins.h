f9504b3cc0829d798ab976964154cbc905b1a7b5
Your Name <you@example.com>
2018-07-23 17:34:03 +0000

    pinmux for LC43

--- bsps/arm/tms570/include/bsp/tms570-pins.h
+++ bsps/arm/tms570/include/bsp/tms570-pins.h
@@ -6,5 +6,9 @@
  * @brief Select pin mapping according to selected chip.
  *        Defaults to TMS570LS3137ZWT for now.
  */
-
+#define TMS570_LC43X
+#if defined (TMS570_LC43X)
+#include <bsp/tms570lc4357-pins.h>
+#else
 #include <bsp/tms570ls3137zwt-pins.h>
+#endif
